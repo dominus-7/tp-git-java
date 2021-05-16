a02ccd7c78e13a2efda3cee629771ee72a44d22b
Mouhamadou <impactdominus7@gmail.com>
2021-05-17 00:07:48 +0100

    Added name property

--- eclipse-workspaceCoursEnLigne/Salle-tp-project1/src/coursenligne/User.java
+++ eclipse-workspaceCoursEnLigne/Salle-tp-project1/src/coursenligne/User.java
@@ -1,5 +1,17 @@
 package coursenligne;
 
 public class User {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 
 }
