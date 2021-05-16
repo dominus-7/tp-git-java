f1788478016b4e2a3d2e18126fd89b41e9a8ad68
Mouhamadou <impactdominus7@gmail.com>
2021-05-17 00:48:45 +0100

    Added salary property

--- eclipse-workspaceCoursEnLigne/Salle-tp-project1/src/coursenligne/User.java
+++ eclipse-workspaceCoursEnLigne/Salle-tp-project1/src/coursenligne/User.java
@@ -4,7 +4,16 @@ public class User {
 	
 	private String name;
 	private int age;
+	private int salary;
 	
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
 	public int getAge() {
 		return age;
 	}
